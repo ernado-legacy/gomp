f1a467c17c5b93d31fc1bd27d58f8c234c29ce74
ernado <ernado@ya.ru>
2014-07-22 13:29:29 +0400

    fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -9,12 +9,11 @@ def update():
     ver = local('git rev-parse HEAD', capture=True)
     with cd(root):
         remote_ver = run('git rev-parse HEAD')
-        print('updating gomp to version %s' % ver)
         run('git reset --hard')
         run('git pull origin master')
         run('sed "s/VERSION/%s/g" Dockerfile.template > Dockerfile' % ver)
         run('docker build -t cydev/gomp .')
         run('docker stop gomp')
         run('docker rm gomp')
-        run('docker run -d --name gomp -v /opt/gomp:/data cydev/gompd')
+        run('docker run -d --name gomp -v /opt/gomp:/data cydev/gomp')
         run('docker restart nginx')
